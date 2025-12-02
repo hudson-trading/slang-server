1c07824f07e9eeb516b97038ad8ab976c4231cde
Andrew Nolte <anolte@hudson-trading.com>
2025-12-02 14:51:57 -0500

    Vscode Debugging: improve configs (#138)
    - Adds auto-attach for user builds
    - Groups launch configurations in debug dropdown
    - Avoids finding its own pid when auto-attaching

--- .vscode/find-pid.sh
+++ .vscode/find-pid.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the path to your server executable
-SERVER_PATH="${1}/build/vscode/bin/slang-server"
+SERVER_PATH="${1}"
 
 # Loop until a PID is found
-PID=$(pgrep -f "${SERVER_PATH}")
+PID=$(pgrep -f "${SERVER_PATH}" | grep -v $$ | head -n 1)
 while [ -z "$PID" ]; do
     sleep 1
-    PID=$(pgrep -f "${SERVER_PATH}")
+    PID=$(pgrep -f "${SERVER_PATH}" | grep -v $$ | head -n 1)
 done
 
 # Print the PID to standard output
