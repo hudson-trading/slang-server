eceb296bb4570f2b1e73387056e1638a6836d88a
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2025-12-26 01:47:20 -0800

    Revert "remove unused listen method"
    This reverts commit 9e36f5021b5b3fd8048999c554fd51e6b55864e2.

--- clients/vscode/src/lib/libconfig.ts
+++ clients/vscode/src/lib/libconfig.ts
@@ -267,9 +267,8 @@ export abstract class ExtensionComponent extends ExtensionNode {
                 viewsInlineButtons.push(button.getButtonWhen())
                 if (button.obj.isSubmenu) {
                   // add submenu number for priority
-                  viewsInlineButtons[
-                    viewsInlineButtons.length - 1
-                  ].group += `@${viewsInlineButtons.length}`
+                  viewsInlineButtons[viewsInlineButtons.length - 1].group +=
+                    `@${viewsInlineButtons.length}`
                 }
               } else if (button instanceof WebviewButton) {
                 webviewButtons.push(button.getButtonWhen())
@@ -397,7 +396,7 @@ interface TreeItemButtonSpec extends ContextCommandSpec {
 // }
 
 export class CommandNode<
-  Spec extends ContextCommandSpec = CommandConfigSpec
+  Spec extends ContextCommandSpec = CommandConfigSpec,
 > extends ExtensionNode {
   obj: Spec
   func: (...args: any[]) => any
@@ -578,6 +577,12 @@ export class ConfigObject<T extends JSONSchemaType> extends ExtensionNode {
   getValue(): T {
     this.cachedValue = vscode.workspace.getConfiguration().get(this.configPath!, this.default!)
     return this.cachedValue
+  }
+
+  listen(): void {
+    this.onConfigUpdated(() => {
+      this.getValue()
+    })
   }
 
   private inferSchema(value: any): any {
