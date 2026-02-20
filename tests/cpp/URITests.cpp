d8efe353c9989a94834900cafdfd8c5467586eef
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-02-20 10:54:06 -0800

    update

--- tests/cpp/URITests.cpp
+++ tests/cpp/URITests.cpp
@@ -1,7 +1,7 @@
 #include "lsp/URI.h"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("URI: Decode") {
+TEST_CASE("URI Decode") {
 
     URI u("file:///x/%41%42%43/%20y.z");
 
@@ -12,8 +12,12 @@ TEST_CASE("URI: Decode") {
 #endif
 }
 
-TEST_CASE("URI: GetPathWithEmptyInput") {
+TEST_CASE("URI EmptyInput") {
     URI u("");
+
+    // TODO: Right now it returns "/" as the URI string.
+    // It should return "/"
+    // CHECK(u.str() == "");
 
 #ifdef _WIN32
     CHECK(u.getPath() == "\\");
@@ -24,16 +28,37 @@ TEST_CASE("URI: GetPathWithEmptyInput") {
 
 #ifdef _WIN32
 
-TEST_CASE("URI: WindowsDriveLetterNormalization") {
-    URI u("file:///c:/temp/file.txt");
+TEST_CASE("URI WindowsDriveLetter") {
+    SECTION("HexDecoded") {
+        URI u("file:///c:/temp/file.txt");
 
-    CHECK(u.getPath() == "C:\\temp\\file.txt");
+        CHECK(u.str() == "file:///C:/temp/file.txt");
+        CHECK(u.getPath() == "C:\\temp\\file.txt");
+    }
+
+    SECTION("HexEncoded") {
+        URI u("file:///c%3A/temp/file.txt");
+
+        CHECK(u.str() == "file:///C:/temp/file.txt");
+        CHECK(u.getPath() == "C:\\temp\\file.txt");
+    }
 }
 
-TEST_CASE("URI: WindowsUNCPath") {
-    URI u("file://server/share/file.txt");
+TEST_CASE("URI WindowsUNCPath") {
+    SECTION("UNC Basic") {
+        URI u("file://server/share/file.txt");
 
-    CHECK(u.getPath() == R"(\\server\share\file.txt)");
+        CHECK(u.str() == "file://server/share/file.txt");
+        CHECK(u.getPath() == R"(\\server\share\file.txt)");
+    }
+
+    SECTION("UNC FromFile") {
+        std::filesystem::path p(R"(\\server\share\file.txt)");
+        URI u = URI::fromFile(p);
+
+        CHECK(u.str() == "file://server/share/file.txt");
+        CHECK(u.getPath() == R"(\\server\share\file.txt)");
+    }
 }
 
 #endif
