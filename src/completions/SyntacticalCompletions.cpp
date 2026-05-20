bc1c2fbd6da869a3e92bf761b3b907d6372066b9
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-05-20 11:24:17 -0700

    add nice little text

--- src/completions/SyntacticalCompletions.cpp
+++ src/completions/SyntacticalCompletions.cpp
@@ -1,5 +1,6 @@
 #include "Snippets.hpp"
 #include "completions/Completions.h"
+#include "lsp/LspTypes.h"
 #include <string_view>
 
 namespace server::completions {
@@ -8,10 +9,10 @@ void addModuleMemberKwCompletions(std::vector<lsp::CompletionItem>& results) {
     for (const auto& snippet : SV_MODULE_MEMBER_SNIPPETS) {
         results.emplace_back(lsp::CompletionItem{
             .label = std::string(snippet.label),
+            .labelDetails = lsp::CompletionItemLabelDetails{"", std::string(snippet.label)},
             .kind = lsp::CompletionItemKind::Snippet,
-            .documentation = snippet.documentation.empty()
-                                 ? std::nullopt
-                                 : std::make_optional(std::string(snippet.documentation)),
+            .documentation = lsp::MarkupContent{lsp::MarkupKind{"markdown"},
+                                                std::string(snippet.documentation)},
             .filterText = std::string(snippet.filterText),
             .insertText = std::string(snippet.insertText),
             .insertTextFormat = lsp::InsertTextFormat::Snippet,
