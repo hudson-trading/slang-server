f7bfaab4aea8a4b95b1fc4af9b143ea0333a81b8
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-08-05 10:29:23 -0700

    Add nix flake (#431)
    Nix flakes create a dev env for people who use nix. This way you
    activate the flake and then your dev env is setup to use and everything.
    
    Stacks onto #430

---
+++ flake.nix
@@ -0,0 +1,53 @@
+# From https://github.com/the-nix-way/dev-templates/blob/main/c-cpp/flake.nix
+{
+	description = "A Nix-flake-based C/C++ development environment";
+
+	inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+	outputs = {self, ...} @ inputs: let
+		supportedSystems = [
+			"x86_64-linux"
+			"aarch64-linux"
+			"aarch64-darwin"
+		];
+		forEachSupportedSystem = f:
+			inputs.nixpkgs.lib.genAttrs supportedSystems (
+				system:
+					f {
+						inherit system;
+						pkgs = import inputs.nixpkgs {inherit system;};
+					}
+			);
+	in {
+		devShells =
+			forEachSupportedSystem (
+				{
+					pkgs,
+					system,
+				}: let
+					llvmPkgs = pkgs.llvmPackages_22;
+				in {
+					default =
+						pkgs.mkShell.override
+						{
+							stdenv = llvmPkgs.stdenv;
+						}
+						{
+							packages = with pkgs;
+								[
+									llvmPkgs.clang-tools
+									cmake
+									doxygen
+									ninja
+									python3
+									boost
+									fmt
+								]
+								++ lib.optionals (!llvmPkgs.stdenv.hostPlatform.isDarwin) [gdb];
+						};
+				}
+			);
+
+		formatter = forEachSupportedSystem ({pkgs, ...}: pkgs.nixfmt);
+	};
+}
