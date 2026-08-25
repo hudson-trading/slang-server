d03d596e428098457b81357d30b5a87fe6fba828
Andrew Nolte <24322714+AndrewNolte@users.noreply.github.com>
2026-08-25 15:13:58 -0400

    fix assignment concatenation highlighting

--- clients/vscode/tests/grammar/snap/multiline_concat_vs_split_instantiation.sv
+++ clients/vscode/tests/grammar/snap/multiline_concat_vs_split_instantiation.sv
@@ -3,16 +3,27 @@ module top;
     logic other_var;
     logic [1:0] this_var;
     logic [3:0] nested_var;
+    logic [7:0] expression_var;
 
-    assign this_var = {
+    assign this_var =
+    {
         some_var,
         other_var
     };
 
-    assign nested_var = {
-        {some_var, other_var},
-        2{some_var}
+    assign nested_var = some_var ? {
+        some_var,
+        other_var
+    } : ~{
+        {some_var, other_var}, 2{some_var}
     };
+
+    assign expression_var = ({
+        make_value(some_var),
+        $bits(other_var),
+        data_type'(this_var),
+        some_pkg::some_value
+    });
 
     some_module
     inst_name
