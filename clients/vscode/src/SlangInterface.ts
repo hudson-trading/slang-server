e1df0937255081b65e1df0bb9fb84a0cd6c54097
Andrew Nolte <24322714+AndrewNolte@users.noreply.github.com>
2026-08-27 18:23:39 -0400

    Expose interface ports in hierarchy

--- clients/vscode/src/SlangInterface.ts
+++ clients/vscode/src/SlangInterface.ts
@@ -14,6 +14,8 @@ export enum SlangKind {
   Instance = 'Instance',
   Scope = 'Scope',
   ScopeArray = 'ScopeArray',
+  InterfacePort = 'InterfacePort',
+  InterfacePortArray = 'InterfacePortArray',
   Param = 'Param',
   Logic = 'Logic',
   Port = 'Port',
@@ -41,6 +43,8 @@ export interface Var extends Item {
 
 // scopes are the only items that can have children
 export interface Scope extends Item {
+  // For interface ports
+  type?: string
   children: Item[]
 }
 
