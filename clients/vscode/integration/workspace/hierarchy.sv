0ab0708ca1017e7909e31ddc6cdc751daf048916
Andrew Nolte <24322714+AndrewNolte@users.noreply.github.com>
2026-09-09 16:29:10 +0100

    Hierarchy View: Add chained hierarchy request (#528)
    Opening a deep hierarchy no longer has to hit the server multiple times
    to resolve

--- clients/vscode/integration/workspace/hierarchy.sv
+++ clients/vscode/integration/workspace/hierarchy.sv
@@ -1,6 +1,18 @@
 module child;
 endmodule
 
+module leaf;
+endmodule
+
+module branch;
+    leaf nested_leaf();
+endmodule
+
 module top;
     child u_child();
+    branch branch_array[1:0]();
+
+    for (genvar i = 0; i < 2; i++) begin : generated
+        leaf generated_leaf();
+    end
 endmodule
